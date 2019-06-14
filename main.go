ac23e2bb36f4f33f3ee2fd0f03fde8ab5534fc13
Vasiliy Matveev <ya-spe@77.88.3.186-spb.dhcp.yndx.net>
2019-06-14 17:35:37 +0300

    better channel

--- main.go
+++ main.go
@@ -18,18 +18,11 @@ func main() {
 	go makeSortSteps(array, drawMergeSort, channel)
 	go makeSortSteps(array, quickSort, channel)
 
-	steps1 := <-channel
+	steps1, steps2, steps3, steps4 := <-channel, <-channel, <-channel, <-channel
 	steps1.setPadding(0, 0)
-
-	steps2 := <-channel
 	steps2.setPadding(200, 0)
-
-	steps3 := <-channel
 	steps3.setPadding(0, 200)
-
-	steps4 := <-channel
 	steps4.setPadding(200, 200)
-
 	steps1.merge(&steps2).merge(&steps3).merge(&steps4)
 
 	dir := "/Users/ya-spe/Downloads"
